3d5d2ac831aa680241dd0481dcf3b5f4cae3c13a
Alexander Maru <marua7665@gmail.com>
2015-03-22 19:33:17 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
-  get 'location' => 'static_pages#location'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
